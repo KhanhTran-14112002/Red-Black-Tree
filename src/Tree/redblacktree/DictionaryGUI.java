3c322ef27b113bfcde817fea855ddb3b15a441d7
DuyNinh2004 <ninhcao2004@gmail.com>
2023-12-05 12:44:15 +0700

    fix

--- src/Tree/redblacktree/DictionaryGUI.java
+++ src/Tree/redblacktree/DictionaryGUI.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 
 public class DictionaryGUI extends JFrame {
-    private RedBlackTree<String, String> dictionary;
+    private RedBlackTree<String, WordInfo> dictionary;
 
     public DictionaryGUI() {
         // Tạo JFrame và cài đặt thuộc tính cơ bản
@@ -37,15 +37,14 @@ public class DictionaryGUI extends JFrame {
         // Đọc dữ liệu từ file và xây dựng từ điển
         dictionary = new RedBlackTree<>();
         buildDictionary("src\\Tree\\redblacktree\\Data");
-
         // Xử lý sự kiện khi nhấn nút "Tra từ"
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String inputWord = textField.getText().trim();
-                RedBlackNode<String,String> definition = dictionary.search(inputWord);
-                if (definition != null) {
-                    resultArea.setText("Nghĩa của \"" + inputWord + "\": " + definition);
+                WordInfo wordInfo = dictionary.search(inputWord).getData();
+                if (wordInfo != null) {
+                    resultArea.setText("Từ: \"" + inputWord + "\"\nLoại từ: " + wordInfo.getType() + "\nNghĩa: " + wordInfo.getDefinition());
                 } else {
                     resultArea.setText("Không tìm thấy từ \"" + inputWord + "\" trong từ điển.");
                 }
@@ -56,17 +55,17 @@ public class DictionaryGUI extends JFrame {
         add(panel, BorderLayout.NORTH);
         add(new JScrollPane(resultArea), BorderLayout.CENTER);
     }
-
-    // Phương thức để đọc dữ liệu từ file và xây dựng từ điển
     private void buildDictionary(String filePath) {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            dictionary = new RedBlackTree<>();
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
-                if (parts.length == 2) {
+                if (parts.length >= 3) {
                     String word = parts[0].trim();
-                    String definition = parts[1].trim();
-                    dictionary.insert(word, definition);
+                    String type = parts[1].trim();
+                    String definition = parts[2].trim();
+                    dictionary.insert(word, new WordInfo(type, definition));
                 } else {
                     System.out.println("Invalid line: " + line);
                 }
@@ -86,6 +85,23 @@ public class DictionaryGUI extends JFrame {
             }
         });
     }
+
+    // Lớp để lưu trữ thông tin về từ
+    private static class WordInfo {
+        private String type;
+        private String definition;
+
+        public WordInfo(String type, String definition) {
+            this.type = type;
+            this.definition = definition;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getDefinition() {
+            return definition;
+        }
+    }
 }
-
-
